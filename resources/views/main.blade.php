00f05a32555accbdab84cc27ff77e54c765a7bd9
romeolumantas <163748464+romeolumantas@users.noreply.github.com>
2025-09-10 03:04:55 +0800

    TEST CICD

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,7 +9,7 @@
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top custom_nav-container" style="background: #fff;">
             <a class="navbar-brand d-flex align-items-center" href="{{ route('landing') }}">
                 <img src="{{ asset('images/logo.png') }}" class="img-fluid rounded-circle me-2" style="height: 40px;" alt="logo"/>
-                <span class="brand-text" style="color: var(--scrolled-color);">Cali-Crane Management Systemssss</span>
+                <span class="brand-text" style="color: var(--scrolled-color);">Cali-Crane Management System</span>
             </a>
 
             <!-- Right-aligned toggle button -->
